38dc04ce6490538695eac003797b42b9f9975593
Pascal Baltazar <pascal@baltazars.org>
2019-05-18 13:54:15 +0200

    adding cams

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -2,17 +2,43 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
+    
+    //cams = new ofVideoGrabber[2];
+    
+    // Camera setup
+    // uncomment to get a list of connected cams:
+    vector<ofVideoDevice> devices = cams[0].listDevices();
+    ofLog() << "Cameras: " ;
+    for (auto& c : devices) ofLog() << c.deviceName << " / ID: " << c.id;
+    
+    cams[0].setDeviceID(1);
+    cams[0].setDesiredFrameRate(30);
+    cams[0].setPixelFormat(OF_PIXELS_NATIVE);
+    cams[0].setup(camWidth, camHeight);
+    
+    cams[1].setDeviceID(0);
+    cams[1].setDesiredFrameRate(30);
+    cams[1].setPixelFormat(OF_PIXELS_NATIVE);
+    cams[1].setup(camWidth, camHeight);
+    
+    ofLog() << "size 1: " << cams[0].getWidth() << " / " << cams[0].getHeight();
+    ofLog() << "size 2: " << cams[1].getWidth() << " / " << cams[1].getHeight();
 
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
+    
+    cams[0].update();
+    cams[1].update();
 
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
 
+    cams[0].draw(300, 100, 620, 680);
+    cams[1].draw(930, 100, 620, 680);
 }
 
 //--------------------------------------------------------------
@@ -25,35 +51,6 @@ void ofApp::keyReleased(int key){
 
 }
 
-//--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseEntered(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseExited(int x, int y){
-
-}
 
 //--------------------------------------------------------------
 void ofApp::windowResized(int w, int h){
@@ -65,7 +62,4 @@ void ofApp::gotMessage(ofMessage msg){
 
 }
 
-//--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
 
-}
--- src/ofApp.h
+++ src/ofApp.h
@@ -3,20 +3,64 @@
 #include "ofMain.h"
 
 class ofApp : public ofBaseApp{
-	public:
-		void setup();
-		void update();
-		void draw();
-		
-		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y);
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void mouseEntered(int x, int y);
-		void mouseExited(int x, int y);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
+public:
+    void setup();
+    void update();
+    void draw();
+    
+    void keyPressed(int key);
+    void keyReleased(int key);
+    void windowResized(int w, int h);
+    void gotMessage(ofMessage msg);
+    
+    ofVideoGrabber cams[2];
+    int camWidth{1920}, camHeight{1080};
+    
+    
+    enum States {
+        INIT,
+        STANDBY,
+        WELCOME,
+        EXPLAIN,
+        QUESTION/*1,
+        QUESTION2,
+        QUESTION3,
+        QUESTION4,
+        QUESTION5,
+        QUESTION6,
+        QUESTION7,
+        QUESTION8,
+        QUESTION9,
+        QUESTION10*/,
+        COMPILING,
+        PROFILE/*1,
+        PROFILE2,
+        PROFILE3,
+        PROFILE4,
+        PROFILE5,
+        PROFILE6,
+        PROFILE7*/,
+        CAM_CHOICE,
+        FRAME,
+        COUNTDOWN,
+        FLASH,
+        RESULT,
+        PRINTING,
+        BYE,
+        END
+    } currentState{};
+    
+    enum Profiles {
+        AGRI,
+        ECO,
+        SPORT,
+        EDUCULT,
+        GEEK,
+        HOMER,
+        SJW
+    } profile{};
+    
+    
+    
+    
 };
