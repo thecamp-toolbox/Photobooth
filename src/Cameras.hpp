fac9fbaf515723e9cf2dd54ded8af54a76f68ad3
Julien Belin <julienbelin002@gmail.com>
2019-11-07 16:29:41 +0100

    Update Cameras.hpp

--- src/Cameras.hpp
+++ src/Cameras.hpp
@@ -21,12 +21,12 @@ public:
     // Tailes des caméras:
     ofParameter<int>
     camXOffset{320}, camYOffset{120},
-    camXsize{640}, camYsize{480};
-    
+    camXsize{360}, camYsize{640};
+
     ofParameter<float>
     brightnessT{2}, contrastT{2},
     brightnessB{1}, contrastB{1};
-    
+
     void setup();
     void setup_GUI();
     void update_one();
@@ -34,7 +34,7 @@ public:
     void draw_one(float x, float y, float w, float h);
     void draw_all(float x1, float y1, float w1, float h1, float x2, float y2, float w2, float h2);
     void exit();
-    
+
     ofVideoGrabber USBCam;
     int USBCamNr{0};
 #ifdef TARGET_RASPBERRY_PI
@@ -44,17 +44,17 @@ public:
     ofVideoGrabber piCam;
     int piCamNr{1};
 #endif
-    
+
     ofShader BCSA_B, BCSA_T;
-    
+
     ofTexture texPicam;
-    
+
     bool current{0};
     int nCams{0};
-    
+
     int maxCamW{1280},
         maxCamH{720};
-    
+
 };
 
 #endif /* Cameras_hpp */
