b92b6a98555e01d6b1230df4cc8e4c8f71d85b57
Julien Belin <julienbelin002@gmail.com>
2019-11-15 13:41:04 +0100

    Update ofApp.cpp

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -246,13 +246,13 @@ void ofApp::update(){
 
                 ofClear(255,255,255, 0);
                 ofSetColor(255,255,255, 255);
-                ofDrawRectangle(0, 0, ticketWidth, ticketHeight+heightLogoClient + profilMarginY);
+                ofDrawRectangle(0, 0, ticketWidth, ticketHeight + (profilMarginY*2));
 
                 profilTicketClient.draw(marginXLogoClient, profilSizeY + ticketMarginYTop + (sizeResCamY*1.5)+ profilMarginY, widthLogoClient, heightLogoClient);
                 result.draw(ticketMarginXLeft, profilSizeY + ticketMarginYTop, sizeResCamX*1.5, sizeResCamY*1.5);
                 profilTicketThecamp.draw(profilMarginX, profilMarginY, profilSizeX, profilSizeY);
 
-                ticket.grabScreen(0, 0, ticketWidth, ticketHeight+heightLogoClient + profilMarginY);
+                ticket.grabScreen(0, 0, ticketWidth, ticketHeight + (profilMarginY*2));
                 ticket.save("/data/screenshot_ticket.png");
 
                 fbo.end();
