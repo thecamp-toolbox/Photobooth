30e386e94285ef236c627e546fd1503e7c48c61a
Julien Belin <julienbelin002@gmail.com>
2019-11-15 11:11:40 +0100

    Update ofApp.cpp

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -249,8 +249,7 @@ void ofApp::update(){
 
                 result.draw(ticketMarginXLeft, profilSizeY   + ticketMarginYTop, sizeResCamX*1.5, sizeResCamY*1.5);
                 profilTicketThecamp.draw(profilMarginX, profilMarginY, profilSizeX, profilSizeY);
-
-                profilTicketClient.draw(marginXLogoClient, profilSizeY + ticketMarginYTop +profilMarginY, widthLogoClient, heightLogoClient);
+                profilTicketClient.draw(marginXLogoClient, profilSizeY + ticketMarginYTop +profilMarginY + (sizeResCamY*1.5)+ profilMarginY, widthLogoClient, heightLogoClient);
 
                 ticket.grabScreen(0, 0, ticketWidth, realTicketHeight);
                 ticket.save("/data/screenshot_ticket.png");
